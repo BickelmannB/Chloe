442473c817935cd3251998080123070389763fb2
bickelmann brice <bickelmann.b@gmail.com>
2020-06-09 09:42:08 +0200

    nouveau module

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
   root to: 'pages#home'
   get '/acceuil', to: "pages#acceuil"
   resources :profils, only: [:index, :show]
-  resources :formations, only: [:index, :show]
+  resources :formations, only: [:index, :show, :new, :create]
   resources :contacts, only: [:new, :create]
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
