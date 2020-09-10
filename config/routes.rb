2aa47929df61a0ac5a0779993784c236253c39af
bickelmann brice <bickelmann.b@gmail.com>
2020-09-10 13:53:42 +0200

    new

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
   root to: 'pages#home'
   get '/acceuil', to: "pages#acceuil"
   resources :profils, only: [:index, :show]
-  resources :formations, only: [:index, :show, :new, :create, :edit, :update]
+  resources :formations, only: [:index, :show]
   resources :contacts, only: [:new, :create]
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
